dfcb5aa351dbc924d498bfe30ebecc92c0baad86
haru2036 <haru2036@gmail.com>
2014-05-17 15:45:04 +0900

    Add: Add member Form

--- Handler/AddMember.hs
+++ Handler/AddMember.hs
@@ -4,15 +4,15 @@ import Import
 
 getAddMemberR :: Handler Html
 getAddMemberR = do 
-  (widget, enctype) <- generateFormPost addMemberForm
+  affiliations <- getAffiliationList
+  (widget, enctype) <- generateFormPost (addMemberForm affiliations)
   defaultLayout $(widgetFile "memberAdd")
 
 postAddMemberR :: Handler Html
 postAddMemberR = error "Not yet implemented: postAddMemberR"
 
-addMemberForm :: Html -> MForm Handler (FormResult Member, Widget)
-addMemberForm = do 
-  aflist <- getAffiliationList
+addMemberForm :: [(Text, Affiliation)] ->  Html -> MForm Handler (FormResult Member, Widget)
+addMemberForm affiliations = do 
   renderDivs $ Member
     <$> areq textField "姓" Nothing
     <*> areq textField "名" Nothing
@@ -20,9 +20,9 @@ addMemberForm = do
     <*> areq textField  "学科記号" Nothing
     <*> areq intField  "学籍番号(下3桁)" Nothing
     <*> areq textField "eMailアドレス" Nothing
-    <*> areq (multiSelectFieldList (aflist)) "チェックボックス(Bool)" Nothing
+    <*> areq (multiSelectFieldList (affiliations)) "チェックボックス(Bool)" Nothing
 
 getAffiliationList = do 
   aflist <-runDB $ selectList [] [Desc AffiliationName]
-  return $ map (\x -> (entityVal x, entityVal x)) $ aflist
+  return $ map (\x -> (affiliationName (entityVal x), entityVal x)) $ aflist
 
