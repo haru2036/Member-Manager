73ae05305e864b8f20e38e8315e58df4b191ebda
haru2036 <haru2036@gmail.com>
2014-05-17 16:09:19 +0900

    Fix: Text

--- Handler/AddMember.hs
+++ Handler/AddMember.hs
@@ -13,16 +13,16 @@ postAddMemberR = error "Not yet implemented: postAddMemberR"
 
 addMemberForm :: [(Text, Affiliation)] ->  Html -> MForm Handler (FormResult Member, Widget)
 addMemberForm affiliations = do 
-  renderDivs $ Member
+  renderBootstrap $ Member
     <$> areq textField "姓" Nothing
     <*> areq textField "名" Nothing
-    <*> areq intField  "入学年" Nothing
+    <*> areq intField  "入学年(下二桁)" Nothing
     <*> areq textField  "学科記号" Nothing
     <*> areq intField  "学籍番号(下3桁)" Nothing
     <*> areq textField "eMailアドレス" Nothing
-    <*> areq (multiSelectFieldList (affiliations)) "チェックボックス(Bool)" Nothing
+    <*> areq (multiSelectFieldList (affiliations)) "所属する班" Nothing
 
 getAffiliationList = do 
-  aflist <-runDB $ selectList [] [Desc AffiliationName]
+  aflist <- runDB $ selectList [] [Desc AffiliationName]
   return $ map (\x -> (affiliationName (entityVal x), entityVal x)) $ aflist
 
