9733bd2853162da1df3049f80e8179076369ce4a
haru2036 <haru2036@gmail.com>
2014-06-07 07:31:13 +0900

    Fix: Delete UCMember when confirms it

--- Handler/ConfirmMember.hs
+++ Handler/ConfirmMember.hs
@@ -5,22 +5,18 @@ import Import
 getConfirmMemberR :: String -> Handler Html
 getConfirmMemberR code = do
   maybeUCMember <- runDB $ getBy $ UniqueConfirm code
-  let maybeMember = shrinkMaybeUCMember maybeUCMember
-  case maybeMember of
-    Just member -> insertMember member
-    Nothing -> lift $ return ()
+  case maybeUCMember of
+    Just uCMember -> confirmMember $ entityVal uCMember
+    Nothing -> lift $ return()
   render <- getMessageRender
   let mayBeMessage = Just MsgMemberConfirmed
   defaultLayout $(widgetFile "info")
 
-shrinkMaybeUCMember :: Maybe (Entity UnConfirmedMember) -> Maybe Member
-shrinkMaybeUCMember (Just member) = Just $ shrinkUCMember $ entityVal member
-shrinkMaybeUCMember Nothing = Nothing
-
 shrinkUCMember :: UnConfirmedMember -> Member
 shrinkUCMember member = unConfirmedMemberMember member
 
---insertMember :: Member -> HandlerT site IO ()
-insertMember member = do 
-  _ <- runDB $ insert member
+confirmMember uCMember = do
+  _ <- runDB $ deleteBy $ UniqueConfirm $ unConfirmedMemberConfirmKey (uCMember :: UnConfirmedMember)
+  _ <- runDB $ insert $ shrinkUCMember uCMember
   lift $ return ()
+
